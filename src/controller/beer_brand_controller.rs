24efc4378d0349ddcaf0b17e8cdbde441c64311d
sarahmariaaclue <81618094+sarahmariaaclue@users.noreply.github.com>
2023-11-03 16:37:34 +0100

    outsource database queries to repository

--- src/controller/beer_brand_controller.rs
+++ src/controller/beer_brand_controller.rs
@@ -1,110 +1,124 @@
+use crate::persistence::beer_brand_persistence::repository::{
+    delete_beer_brand_by_id, find_beer_brand_by_id, find_beer_brands, save_beer_brand,
+    save_updated_beer_brand, DatabaseError,
+};
 use crate::persistence::beer_brand_persistence::BeerBrandEntity;
 use crate::FunnyEndpointsDB;
 
 use rocket::serde::json::Json;
 use rocket_db_pools::Connection;
-use serde::{Deserialize, Serialize};
-use sqlx::{self};
+use serde::Deserialize;
 
 use rocket::http::Status;
 use rocket::request::Request;
 use rocket::response::{Responder, Response};
-use std::i64;
 use std::io::Cursor;
-// TODO datenbanck kram auslagern in repository
-// TODO Fehlerbehandlung
 
 #[get("/beer-brands")]
 pub async fn get_beer_brands(
-    mut connection: Connection<FunnyEndpointsDB>,
-) -> Option<Json<Vec<BeerBrandEntity>>> {
-    let result = sqlx::query_as::<_, BeerBrandEntity>("SELECT * FROM beer_brand")
-        .fetch_all(&mut *connection)
-        .await
-        .ok()?;
-    Some(Json(result))
+    connection: Connection<FunnyEndpointsDB>,
+) -> Result<Json<Vec<BeerBrandEntity>>, DatabaseError> {
+    let result = find_beer_brands(connection).await;
+    result.map(Json)
 }
 
 #[get("/beer-brand/<id>")]
 pub async fn get_beer_brand(
     id: i64,
-    mut connection: Connection<FunnyEndpointsDB>,
-) -> Option<Json<BeerBrandEntity>> {
-    let result = sqlx::query_as::<_, BeerBrandEntity>("SELECT * FROM beer_brand WHERE id = $1")
-        .bind(id)
-        .fetch_optional(&mut *connection)
-        .await
-        .ok()?
-        .map(Json)?;
-    Some(result)
+    connection: Connection<FunnyEndpointsDB>,
+) -> Result<Option<Json<BeerBrandEntity>>, DatabaseError> {
+    let result = find_beer_brand_by_id(connection, id).await;
+    result.map(|option| option.map(Json))
 }
 
 #[post("/beer-brand", data = "<beer_brand>")]
 pub async fn create_beer_brand(
-    mut connection: Connection<FunnyEndpointsDB>,
+    connection: Connection<FunnyEndpointsDB>,
     beer_brand: Json<BeerBrandDto>,
 ) -> Result<Json<BeerBrandEntity>, DatabaseError> {
-    let result = sqlx::query_as::<_, BeerBrandEntity>(
-        "INSERT INTO beer_brand (name, city) VALUES ($1, $2) RETURNING *",
-    )
-    .bind(&beer_brand.name)
-    .bind(&beer_brand.city)
-    .fetch_one(&mut *connection)
-    .await
-    .map_err(DatabaseError)?;
-
-    Ok(Json(result))
+    let result = save_beer_brand(connection, &beer_brand.name, &beer_brand.city).await;
+    result.map(Json)
 }
 
 #[put("/beer-brand", data = "<beer_brand_entity>")]
 pub async fn update_beer_brand(
-    mut connection: Connection<FunnyEndpointsDB>,
+    connection: Connection<FunnyEndpointsDB>,
     beer_brand_entity: Json<BeerBrandEntity>,
-) -> Result<Json<BeerBrandEntity>, DatabaseError> {
-    let result = sqlx::query_as::<_, BeerBrandEntity>(
-        "UPDATE beer_brand SET name = $2, city = $3 WHERE id= $1 RETURNING *",
-    )
-    .bind(beer_brand_entity.get_id())
-    .bind(beer_brand_entity.get_name())
-    .bind(beer_brand_entity.get_city())
-    .fetch_one(&mut *connection)
-    .await
-    .map_err(DatabaseError)?;
-
-    Ok(Json(result))
+) -> Result<Option<Json<BeerBrandEntity>>, DatabaseError> {
+    let result = save_updated_beer_brand(connection, beer_brand_entity.into_inner()).await;
+    result.map(|option| option.map(Json))
 }
 
 #[delete("/beer-brand/<id>")]
 pub async fn delete_beer_brand(
-    mut connection: Connection<FunnyEndpointsDB>,
+    connection: Connection<FunnyEndpointsDB>,
     id: i64,
-) -> Result<Json<BeerBrandEntity>, DatabaseError> {
-    let result =
-        sqlx::query_as::<_, BeerBrandEntity>("DELETE FROM beer_brand WHERE id= $1 RETURNING *")
-            .bind(id)
-            .fetch_one(&mut *connection)
-            .await
-            .map_err(DatabaseError)?;
-
-    Ok(Json(result))
+) -> Result<Option<Json<BeerBrandEntity>>, DatabaseError> {
+    let result: Result<Option<BeerBrandEntity>, DatabaseError> =
+        delete_beer_brand_by_id(connection, id).await;
+    result.map(|option| option.map(Json))
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct BeerBrandDto {
     name: String,
     city: String,
 }
 
-#[derive(Debug)]
-pub struct DatabaseError(sqlx::Error);
-
 impl<'r> Responder<'r, 'static> for DatabaseError {
     fn respond_to(self, _: &'r Request<'_>) -> Result<Response<'static>, Status> {
-        let message = format!("Database error: {}", self.0);
+        let message = self.get_message();
         let response = Response::build()
             .status(Status::InternalServerError)
             .sized_body(message.len(), Cursor::new(message))
             .finalize();
         Ok(response)
+    }
+}
+
+// Construct a Rocket instance that represents the application.
+
+// 1
+// let rocket = rocket::build();
+// Construct a Client using the Rocket instance.
+
+// 1
+// let client = Client::tracked(rocket).unwrap();
+// Construct requests using the Client instance.
+
+// 1
+// let req = client.get("/");
+// Dispatch the request to retrieve the response.
+
+// 1
+// let response = req.dispatch();
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::rocket;
+    use rocket::local::asynchronous::Client;
+
+    #[rocket::async_test]
+    async fn test_get_beer_brands() {
+        //    let should_be = "Moin!";
+        let rocket = rocket().await;
+        let client = Client::tracked(rocket).await.unwrap();
+        let request = client.get("/beer-brands");
+        let response = request.dispatch().await;
+
+        // let rocket = rocket::build();
+        // let client = Client::tracked(rocket).unwrap();
+        // let request = client.get("/beer-brands");
+        // let response = request.dispatch();
+
+        // mock the database
+
+        // #[derive(Database)]
+        // #[database("funny_endpoints")]
+        // pub struct FunnyEndpointsDB(sqlx::Pool<Postgres>);
+
+        assert_eq!(response.status(), Status::Ok);
     }
 }
--- src/persistence/beer_brand_persistence.rs
+++ src/persistence/beer_brand_persistence.rs
@@ -24,3 +24,83 @@ impl BeerBrandEntity {
         Self { id, name, city }
     }
 }
+
+pub mod repository {
+
+    use super::*;
+    use crate::FunnyEndpointsDB;
+    use rocket_db_pools::Connection;
+    use sqlx::{self};
+
+    pub async fn find_beer_brands(
+        mut connection: Connection<FunnyEndpointsDB>,
+    ) -> Result<Vec<BeerBrandEntity>, DatabaseError> {
+        let find_all_statement = "SELECT * FROM beer_brand";
+        sqlx::query_as::<_, BeerBrandEntity>(find_all_statement)
+            .fetch_all(&mut *connection)
+            .await
+            .map_err(DatabaseError)
+    }
+
+    pub async fn find_beer_brand_by_id(
+        mut connection: Connection<FunnyEndpointsDB>,
+        id: i64,
+    ) -> Result<Option<BeerBrandEntity>, DatabaseError> {
+        let find_by_id_statement = "SELECT * FROM beer_brand WHERE id = $1";
+        sqlx::query_as::<_, BeerBrandEntity>(find_by_id_statement)
+            .bind(id)
+            .fetch_optional(&mut *connection)
+            .await
+            .map_err(DatabaseError)
+    }
+
+    pub async fn save_beer_brand(
+        mut connection: Connection<FunnyEndpointsDB>,
+        beer_brand_name: &str, // doch dto übergeben?
+        beer_brand_city: &str,
+    ) -> Result<BeerBrandEntity, DatabaseError> {
+        let save_new_statement = "INSERT INTO beer_brand (name, city) VALUES ($1, $2) RETURNING *";
+        sqlx::query_as::<_, BeerBrandEntity>(save_new_statement)
+            .bind(beer_brand_name)
+            .bind(beer_brand_city)
+            .fetch_one(&mut *connection)
+            .await
+            .map_err(DatabaseError)
+    }
+
+    pub async fn save_updated_beer_brand(
+        mut connection: Connection<FunnyEndpointsDB>,
+        beer_brand_entity: BeerBrandEntity,
+    ) -> Result<Option<BeerBrandEntity>, DatabaseError> {
+        let update_statement =
+            "UPDATE beer_brand SET name = $2, city = $3 WHERE id= $1 RETURNING *";
+        sqlx::query_as::<_, BeerBrandEntity>(update_statement)
+            .bind(beer_brand_entity.get_id())
+            .bind(beer_brand_entity.get_name())
+            .bind(beer_brand_entity.get_city())
+            .fetch_optional(&mut *connection)
+            .await
+            .map_err(DatabaseError)
+    }
+
+    pub async fn delete_beer_brand_by_id(
+        mut connection: Connection<FunnyEndpointsDB>,
+        id: i64,
+    ) -> Result<Option<BeerBrandEntity>, DatabaseError> {
+        let delete_statement = "DELETE FROM beer_brand WHERE id = $1 RETURNING *";
+        sqlx::query_as::<_, BeerBrandEntity>(delete_statement)
+            .bind(id)
+            .fetch_optional(&mut *connection)
+            .await
+            .map_err(DatabaseError)
+    }
+
+    #[derive(Debug)]
+    pub struct DatabaseError(sqlx::Error);
+
+    impl DatabaseError {
+        pub fn get_message(&self) -> String {
+            format!("Database error: {}", self.0)
+        }
+    }
+}
